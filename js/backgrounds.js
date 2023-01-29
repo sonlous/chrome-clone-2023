5aa96494d2ee88ccdfd9aaed9772aa675fa5a3e6
woojiiiiiin <sonujin65@gmail.com>
2023-01-30 02:19:50 +0900

    1.0.0
    Applying CSS

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,5 +1,5 @@
 const images = [
-    "0.jpg", "1.jpg", "2.jpg"
+    "0.png", "1.png", "2.png", "3.png", "4.png", "5.png"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,43 @@
 const quotes = [
     {
-        quote: "침묵하는 사람보다 언어에 능숙한 사람은 없다.",
-        author: "샘 레이번"
+        quote: "No one has a finer command of language than the person who keeps his mouth shut.",
+        author: "- Sam Rayburn"
     },
     {
-        quote: "아름다움에 결점이 없다는 것 자체가 결점이다.",
-        author: "해브록 엘리스"
+        quote: "The absence of flaw in beauty is itself a flaw.",
+        author: "- Havelock Ellis"
     },
     {
-        quote: "인생은 거짓된 상황의 끝없는 연속이다.",
-        author: "소온톤 와일더"
+        quote: "Life is an unbroken succession of false situations.",
+        author: "- Thornton Wilder"
     },
     {
-        quote: "인생이 진지하다는 증거는 털끝만큼도 없다.",
-        author: "브렌단 길"
+        quote: "Not a shred of evidence exists in favor of the idea that life is serious.",
+        author: "- Brendan Gill"
     },
     {
-        quote: "인생은 겸손에 대한 오랜 수업이다.",
-        author: "제임스 M. 베리"
+        quote: "Life is a long lesson in humility.",
+        author: "- James M. Barrie"
     },
     {
-        quote: "인생의 목적은 성숙하지 않기 위해 싸우는 것이다.",
-        author: "딕 워트하이머"
+        quote: "The purpose of life is to fight maturity.",
+        author: "- Dick Werthimer"
     },
     {
-        quote: "완벽함이 아니라 탁월함을 위해서 애써라.",
-        author: "H. 잭슨 브라운 주니어"
+        quote: "Strive for excellence, not perfection.",
+        author: "- H. Jackson Brown Jr."
     },
     {
-        quote: "언제나 현재에 집중할 수 있다면 행복할 것이다.",
-        author: "루이 파스퇴르"
+        quote: "If you can concentrate always on the present, you'll be a happy man.",
+        author: "- Paulo Cuelho"
     },
     {
-        quote: "침묵하는 사람보다 언어에 능숙한 사람은 없다.",
-        author: "파울로 코엘료"
+        quote: "Dream as if you'll live forever. Live as if you'll die today.",
+        author: "- James Dean"
     },
     {
-        quote: "이른 아침은 입에 황금을 물고 있다.",
-        author: "벤자민 프랭클린"
+        quote: "Early morning hath gold in its mouth.",
+        author: "- Benjamin Franklin"
     }
 ];
 
